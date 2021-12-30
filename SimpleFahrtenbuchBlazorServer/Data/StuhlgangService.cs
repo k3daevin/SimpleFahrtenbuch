f1f6403b4023475de7a0417200ca0f854fd9fc8f
k3daevin <kevindrzyc@gmail.com>
2021-12-30 18:36:19 +0100

    blazor reingebollert

---
+++ SimpleFahrtenbuchBlazorServer/Data/StuhlgangService.cs
@@ -0,0 +1,17 @@
+﻿namespace SimpleFahrtenbuchBlazorServer.Data
+{
+    public class StuhlgangService
+    {
+        private List<Stuhlgang> Stuhlgangs = new List<Stuhlgang>()
+        {
+            new Stuhlgang {TimeStamp = DateTime.Now - TimeSpan.FromHours(2), BristolSkala = 3, Kommentar = "stabil"},
+            new Stuhlgang {TimeStamp = DateTime.Now - TimeSpan.FromHours(1), BristolSkala = 7, Kommentar = "pisse aus dem Arsch"},
+            new Stuhlgang {TimeStamp = DateTime.Now - TimeSpan.FromHours(-1), BristolSkala = 1, Kommentar = "future pflock"},
+        };
+
+        public IEnumerable<Stuhlgang> GetStuhlgangs() => Stuhlgangs;
+
+
+        public void AddStuhlgang(Stuhlgang stuhlgang) => Stuhlgangs.Add(stuhlgang);
+    }
+}
